4b06ace33e3ebdaa2a5b7234301fe01ba0a6661a
MyungHyun-Ahn <ahn020367@gmail.com>
2022-11-07 20:05:50 +0900

    업데이트

--- datafile.py
+++ datafile.py
@@ -1,12 +1,15 @@
 import pygame, os
 
 DIR_PATH = os.path.dirname(__file__)
-DIR_IMAGE = os.path.join(DIR_PATH, 'image')
-DIR_SOUND = os.path.join(DIR_PATH, 'sound')
+DIR_IMAGE = os.path.join(DIR_PATH, 'resource/image')
+DIR_SOUND = os.path.join(DIR_PATH, 'resource/sound')
 
 class SpriteSheet:
     def __init__(self, filename, width, height, max_row, max_col, max_index) -> None:
+        # 스프레드시트 이미지 로드
         baseImage = pygame.image.load(os.path.join(DIR_IMAGE, filename))
+
+        # 스프레드시트를 자르고 하나씩 추가할 리스트
         self.spr = []
 
         for i in range(max_index):
@@ -14,3 +17,5 @@ class SpriteSheet:
             image.blit(baseImage, (0, 0), ((i % width) * width, (i / max_row) * height, width, height))
             image_scaled = pygame.transform.scale(image, (width * 4, height * 4))
             self.spr.append(image_scaled)
+
+        
