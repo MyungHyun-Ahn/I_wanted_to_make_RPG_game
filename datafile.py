98d5b9705db43bc8762c0790221dd71290d21d09
MyungHyun-Ahn <ahn020367@gmail.com>
2022-11-07 11:46:58 +0900

    datafile.py 생성

---
+++ datafile.py
@@ -0,0 +1,16 @@
+import pygame, os
+
+DIR_PATH = os.path.dirname(__file__)
+DIR_IMAGE = os.path.join(DIR_PATH, 'image')
+DIR_SOUND = os.path.join(DIR_PATH, 'sound')
+
+class SpriteSheet:
+    def __init__(self, filename, width, height, max_row, max_col, max_index) -> None:
+        baseImage = pygame.image.load(os.path.join(DIR_IMAGE, filename))
+        self.spr = []
+
+        for i in range(max_index):
+            image = pygame.Surface((width, height))
+            image.blit(baseImage, (0, 0), ((i % width) * width, (i / max_row) * height, width, height))
+            image_scaled = pygame.transform.scale(image, (width * 4, height * 4))
+            self.spr.append(image_scaled)
